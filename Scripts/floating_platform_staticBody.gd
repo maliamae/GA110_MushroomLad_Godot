438ba248dbc8580e7af454363217dfad0b5d5423
maliamae <maliakephart@lcad.edu>
2026-03-16 23:34:21 -0700

    fixed floating

--- Scripts/floating_platform_staticBody.gd
+++ Scripts/floating_platform_staticBody.gd
@@ -12,7 +12,8 @@ var reseting: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	initial_pos = get_global_position()
+	initial_pos = global_position
+	print(initial_pos)
 	max_height = initial_pos.y
 	min_height = max_height - move_distance
 	oscilate_t = randf_range(0.0, 1.0)
@@ -24,7 +25,7 @@ func _process(delta: float) -> void:
 
 
 func oscilate_height(delta):
-	global_position = Vector3(initial_pos.x, lerpf(max_height, min_height, oscilate_t),initial_pos.y)
+	global_position = Vector3(initial_pos.x, lerpf(max_height, min_height, oscilate_t),initial_pos.z)
 	oscilate_t += 0.01 * move_speed * delta
 	
 	if (oscilate_t > 1.0):
