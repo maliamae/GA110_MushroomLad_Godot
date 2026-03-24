8f2fe71ad7334e83c46e9d2bf58529240368c303
maliamae <maliakephart@lcad.edu>
2026-03-23 23:33:24 -0700

    commenting

--- Scripts/floating_platform.gd
+++ Scripts/floating_platform.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+#floating barrels that sink when player contacts them
+
 var initial_pos: Vector3 
 var max_height: float
 var min_height: float
@@ -11,51 +13,55 @@ var reset_t = 0.0
 var is_under_player: bool = false
 var reseting: bool = false
 
-@onready var platform: Node3D = $".."
+@onready var platform: Node3D = $".." #reference to parent node of barrel this script is attached to
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	initial_pos = platform.global_position
+	#save initial position and set max/min heights
+	initial_pos = platform.global_position 
 	max_height = initial_pos.y
 	min_height = max_height - move_distance
-	oscilate_t = randf_range(0.0, 1.0)
+	oscilate_t = randf_range(0.0, 1.0) #gives random variation of starting time in oscilation sequence
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if not is_under_player && not reseting:
+		#oscilate like normal object floating on water
 		oscilate_height(delta)
 	elif not is_under_player && reseting:
 		#reset platform to original position
 		reset_pos(delta)
 	else:
 		#player is on platform and platform is sinking
-		#print("sinking")
 		platform.global_position -= Vector3.UP * (move_speed * 0.01) * delta
 	
+	#if platform exceeds depth of min height * 1.5, begin reseting to initial position
 	if platform.global_position.y < (initial_pos.y - move_distance * 1.5) && not reseting:
 		reseting = true
 		is_under_player = false
-		#print("start reseting")
 
-
+#moves platform up and down constantly
 func oscilate_height(delta):
 	platform.global_position = Vector3(initial_pos.x, lerpf(max_height, min_height, oscilate_t),initial_pos.z)
 	oscilate_t += 0.01 * move_speed * delta
 	
+	#set reset time to 0 to ensure that when platform begins reseting it begins from it's expected height
 	reset_t = 0.0
 	
+	#once target position is reached, values of the max/min height are swapped and the cycle continues
 	if (oscilate_t > 1.0):
 		var temp = max_height
 		max_height = min_height
 		min_height = temp
 		oscilate_t = 0.0
 
-
+#registers player coming into contact to begin sinking
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
 		is_under_player = true
 
+#returns platform to last target position in oscilation cycle
 func reset_pos(delta):
 	#print("reseting")
 	var current_y = initial_pos.y - move_distance * 1.5
@@ -63,6 +69,7 @@ func reset_pos(delta):
 	reset_t += 0.01 * move_speed * delta
 	
 	oscilate_t = 0.0
-		
+	
+	#ends reseting cycle
 	if (reset_t > 1.0):
 		reseting = false
